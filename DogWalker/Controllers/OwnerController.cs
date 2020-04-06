ccde99b1331139d09a721ae31bb81881a89707c4
Brenda Long <brenda@studio6thave.com>
2020-04-06 15:55:37 -0500

    added put/update for owner

--- DogWalker/Controllers/OwnerController.cs
+++ DogWalker/Controllers/OwnerController.cs
@@ -1,4 +1,5 @@
 ﻿using DogWalker.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
@@ -112,6 +113,100 @@ namespace DogWalker.Controllers
             }
         }
         //end get by id
+
+
+        // post new owner
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Owner owner)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO DogOwner (OwnerName, NeighborhoodId, OwnerAddress, PhoneNumber)
+                    OUTPUT INSERTED.Id
+                    VALUES (@OwnerName,@NeighborhoodId, @OwnerAddress, @PhoneNumber)";
+                    cmd.Parameters.Add(new SqlParameter("@OwnerName", owner.Name));
+                    cmd.Parameters.Add(new SqlParameter("@NeighborhoodId", owner.NeighborhoodId));
+                    cmd.Parameters.Add(new SqlParameter("@OwnerAddress", owner.Address));
+                    cmd.Parameters.Add(new SqlParameter("@PhoneNumber", owner.PhoneNumber));
+                   
+                    int newId = (int)cmd.ExecuteScalar();
+                    owner.Id = newId;
+                    return CreatedAtRoute("GetOwner", new { id = newId }, owner);
+                }
+            }
+        }
+        //end post new owner
+
+        //edit owner
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Owner owner)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                                            UPDATE DogOwner 
+                                            SET OwnerName = @Name, 
+                                            OwnerAddress = @Address,
+                                            NeighborhoodId = @NeighborhoodId, 
+                                            PhoneNumber = @PhoneNumber
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@Name", owner.Name));
+                        cmd.Parameters.Add(new SqlParameter("@Address", owner.Address));
+                        cmd.Parameters.Add(new SqlParameter("@NeighborhoodId", owner.NeighborhoodId));
+                        cmd.Parameters.Add(new SqlParameter("@PhoneNumber", owner.PhoneNumber));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!OwnerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //end edit owner
+
+        private bool OwnerExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, OwnerName
+                    FROM DogOwner
+                    WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
 
     } //end controller class
 } //end controller
