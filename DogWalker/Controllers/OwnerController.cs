b4f98be1548676c9b4c8312fbfd93bc1bad91bcd
Brenda Long <brenda@studio6thave.com>
2020-04-06 14:38:32 -0500

    added owner controller get

---
+++ DogWalker/Controllers/OwnerController.cs
@@ -0,0 +1,117 @@
+﻿using DogWalker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWalker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+          
+        public OwnerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, OwnerName, OwnerAddress, PhoneNumber, NeighborhoodId
+                    FROM DogOwner";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Owner> OwnerList = new List<Owner>();
+
+                    while (reader.Read())
+                    {
+                        Owner owner = new Owner
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("OwnerName")),
+                            PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                            Address = reader.GetString(reader.GetOrdinal("OwnerAddress")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        
+
+                        };
+
+                        OwnerList.Add(owner);
+                    }
+
+                    reader.Close();
+                    return Ok(OwnerList);
+                }
+            }
+        } //end get
+
+        //get by id
+        [HttpGet("{id}", Name = "GetOwner")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT o.Id, o.OwnerName, o.OwnerAddress, o.PhoneNumber, o.NeighborhoodId,
+                    n.NeighborhoodName, n.Id AS NId
+                    FROM DogOwner o
+                    Join Neighborhood n On o.NeighborhoodId = n.Id
+                    WHERE o.Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Owner owner = null;
+
+                    if (reader.Read())
+                    {
+                        owner = new Owner
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("OwnerName")),
+                            PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                            Address = reader.GetString(reader.GetOrdinal("OwnerAddress")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NId"))
+                        };
+
+                        owner.Neighborhood = new Neighborhood
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("NeighborhoodName"))
+
+                        };
+
+                    }
+                    reader.Close();
+                    return Ok(owner);
+                }
+            }
+        }
+        //end get by id
+
+    } //end controller class
+} //end controller
--- DogWalker/Models/Owner.cs
+++ DogWalker/Models/Owner.cs
@@ -9,8 +9,10 @@ namespace DogWalker.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int NeighborhoodId { get; set; }
+        public string Address { get; set; }
         public string PhoneNumber { get; set; }
+        public int NeighborhoodId { get; set; }
+        public Neighborhood Neighborhood { get; set; }
         public List<Dog> Dogs { get; set; }
     }
 }
